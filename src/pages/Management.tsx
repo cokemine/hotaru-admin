72c22d77114eb78f02b8efff36a782dde3a6254c
cokemine <aptx4561@gmail.com>
2021-09-24 19:40:54 +0800

    refactor: remove unused useMemo

--- src/pages/Management.tsx
+++ src/pages/Management.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactElement, useState, useMemo } from 'react';
+import React, { FC, ReactElement, useState } from 'react';
 
 import { Typography, Table, Tag, Modal, Input, Form, Switch, Button, AutoComplete } from 'antd';
 import { ColumnsType } from 'antd/es/table';
